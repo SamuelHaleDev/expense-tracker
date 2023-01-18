d76826c40fcf181764cb6df954094b80e047111b
Samuel Hale <samuelhalebusiness@gmail.com>
2023-01-18 12:56:07 -0500

    Initial commit

---
+++ expense-tracker-react/src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// Reducer is basicall how we specify application state changes in response to certain actions
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
